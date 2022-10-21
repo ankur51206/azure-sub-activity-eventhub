ed4e09c7a1bbc92d91ecef13af4e298b23b87a36
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2022-10-21 20:59:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ data "azurerm_subscription" "current" {}
 
 
 resource "azurerm_monitor_diagnostic_setting" "diag-setting" {
-  name                           = "toEventHub"
+  name                           = var.name
   target_resource_id             = data.azurerm_subscription.current.id
-  eventhub_name                  = "subscriptionactivitylog"
+  eventhub_name                  = var.eventhub_name
   eventhub_authorization_rule_id = "/subscriptions/f3d20c9f-3cb5-45df-b6a8-32f7f4e3d1b6/resourceGroups/sample-1/providers/Microsoft.EventHub/namespaces/eventnamespaceankur/authorizationRules/RootManageSharedAccessKey"
 
   log {
