9573d88cd5f1396ee491243492a14e8b75a2a746
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2022-10-25 18:19:26 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -9,3 +9,18 @@ variable "eventhub_name" {
   type        = string
   default     = "subscriptionactivitylog"
 }
+
+
+
+
+variable "eventhub_rule_id" {
+  type        = map
+  default     = {
+		eastus2 = {
+			cloudops = "/subscriptions/f3d20c9f-3cb5-45df-b6a8-32f7f4e3d1b6/resourceGroups/sample-1/providers/Microsoft.EventHub/namespaces/myeventusankur/authorizationRules/RootManageSharedAccessKey"
+			}
+		eastus = {
+			cloudops = "/subscriptions/f3d20c9f-3cb5-45df-b6a8-32f7f4e3d1b6/resourceGroups/sample-1/providers/Microsoft.EventHub/namespaces/eventnamespaceankur/authorizationRules/RootManageSharedAccessKey"
+			}
+	}
+}
